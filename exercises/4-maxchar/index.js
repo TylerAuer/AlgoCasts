1d828471291db4a54765a771dbd80ab68772078c
Tyler Auer <Tyler.Auer@gmail.com>
2020-05-07 13:31:57 -0700

    solved

--- exercises/4-maxchar/index.js
+++ exercises/4-maxchar/index.js
@@ -5,6 +5,38 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 // maxChar("bbaa") === "ab" not "ba"
-function maxChar(str) {}
+
+// 1 - Build Object
+function maxChar(str) {
+  const arr = str.split("");
+
+  const charCounts = {};
+  for (char of arr) {
+    // if key exists => increment
+    if (charCounts[char]) {
+      charCounts[char] += 1;
+      // if key doesn't exist => create
+    } else {
+      charCounts[char] = 1;
+    }
+  }
+  // determine max count
+  let maxCount = 0;
+  for (char in charCounts) {
+    if (charCounts[char] > maxCount) {
+      maxCount = charCounts[char];
+    }
+  }
+  const maxArr = [];
+  // build arr then str of maxChar(s)
+  for (char in charCounts) {
+    if (charCounts[char] === maxCount) {
+      maxArr.push(char);
+    }
+  }
+  return maxArr.sort().join("");
+}
+
+console.log(maxChar("FFFaaabbbCCC"));
 
 module.exports = maxChar;
