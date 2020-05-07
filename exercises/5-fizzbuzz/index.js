eea0a6aecd41caac5e06c28fcb7ff4b851c3d0e2
Tyler Auer <Tyler.Auer@gmail.com>
2020-05-07 16:05:32 -0700

    added a solution without an arrow function

--- exercises/5-fizzbuzz/index.js
+++ exercises/5-fizzbuzz/index.js
@@ -12,6 +12,38 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// 1 - Build a string
+// function fizzBuzz(n) {
+//   iterator = (number) => {
+//     let str = "";
+//     if (number % 3 === 0) {
+//       str += "fizz";
+//     }
+//     if (number % 5 === 0) {
+//       str += "buzz";
+//     }
+
+//     return str ? str : number;
+//   };
+
+//   // call and log for numbers 1 to n
+//   for (let i = 1; i <= n; i++) {
+//     console.log(iterator(i));
+//   }
+// }
+
+// 2 - Build a string inside for loop
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    let str = "";
+    if (i % 3 === 0) {
+      str += "fizz";
+    }
+    if (i % 5 === 0) {
+      str += "buzz";
+    }
+    console.log(str ? str : i);
+  }
+}
 
 module.exports = fizzBuzz;
