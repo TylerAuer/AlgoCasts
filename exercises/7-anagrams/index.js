621be5e1c066c47da5099c3cda63e002e4ed2b08
Tyler Auer <Tyler.Auer@gmail.com>
2020-05-11 13:45:04 -0700

    add refined solution to anagrams

--- exercises/7-anagrams/index.js
+++ exercises/7-anagrams/index.js
@@ -9,42 +9,75 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 // 1 - First attempt
-function anagrams(stringA, stringB) {
-  // removes punctuation and spaces
-  removeNonChars = (str) => str.replace(/[\W]/g, "").toLowerCase();
-  const strA = removeNonChars(stringA);
-  const strB = removeNonChars(stringB);
+// function anagrams(stringA, stringB) {
+//   // removes punctuation and spaces
+//   removeNonChars = (str) => str.replace(/[\W]/g, "").toLowerCase();
+//   const strA = removeNonChars(stringA);
+//   const strB = removeNonChars(stringB);
 
-  // Ensures StringA and StringB are the same length once
-  // punctuation and spaces are removed.
-  // This also lets us iterate over stringA's keys and compare
-  // values in A and B. Without this, for example,
-  // "aaa" would appear to match "aaab"
-  // since it would just check a[a] and b[a] and they would match
-  if (strA.length != strB.length) {
+//   // Ensures StringA and StringB are the same length once
+//   // punctuation and spaces are removed.
+//   // This also lets us iterate over stringA's keys and compare
+//   // values in A and B. Without this, for example,
+//   // "aaa" would appear to match "aaab"
+//   // since it would just check a[a] and b[a] and they would match
+//   if (strA.length != strB.length) {
+//     return false;
+//   }
+
+//   // Maps characters to object
+//   toObj = (str) => {
+//     const obj = {};
+//     for (let char of str) {
+//       obj[char] = obj[char] + 1 || 1;
+//     }
+//     return obj;
+//   };
+
+//   const a = toObj(strA);
+//   const b = toObj(strB);
+
+//   for (key of Object.keys(a)) {
+//     if (a[key] !== b[key]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// 2 - Revised using for...in and comparing the lenght of the Object.keys()
+// instead of comparing string lengths
+
+function anagrams(stringA, stringB) {
+  const makeCharMap = (str) => {
+    const charMap = {};
+    for (let char of str.replace(/[\W]/g, "").toLowerCase()) {
+      charMap[char] = charMap[char] + 1 || 1;
+    }
+    return charMap;
+  };
+
+  const aMap = makeCharMap(stringA);
+  const bMap = makeCharMap(stringB);
+
+  console.log(aMap);
+  console.log(bMap);
+
+  // Check that they have the same number of keys
+  if (Object.keys(aMap).length !== Object.keys(bMap).length) {
     return false;
   }
 
-  // Maps characters to object
-  toObj = (str) => {
-    const obj = {};
-    for (let char of str) {
-      obj[char] = obj[char] + 1 || 1;
-    }
-    return obj;
-  };
-
-  const a = toObj(strA);
-  const b = toObj(strB);
-
-  for (key of Object.keys(a)) {
-    if (a[key] != b[key]) {
+  // Check the each char appears the same number of times
+  for (let char in aMap) {
+    if (aMap[char] !== bMap[char]) {
       return false;
     }
   }
+
   return true;
 }
 
-anagrams("A tree, a life, a bench", "A tree, a fence, a yard");
+anagrams("Whoa! Hi!", "Hi! Whoa!");
 
 module.exports = anagrams;
