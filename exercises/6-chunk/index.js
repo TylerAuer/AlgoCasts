fbaca279c63e4b689cd3aa27cd832411ecd7aab9
Tyler Auer <Tyler.Auer@gmail.com>
2020-05-11 12:04:18 -0700

    solve chunk

--- exercises/6-chunk/index.js
+++ exercises/6-chunk/index.js
@@ -8,6 +8,50 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// Solution 1
+// function chunk(array, size) {
+//   const origArr = array;
+//   const returnArr = [];
+
+//   // Makes subarrays of length size
+//   while (origArr.length >= size) {
+//     const nestedReturnArr = [];
+//     for (let i = 0; i < size; i++) {
+//       nestedReturnArr.push(origArr.shift());
+//     }
+//     returnArr.push(nestedReturnArr);
+//   }
+
+//   // Checks if there are any leftover numbers
+//   if (origArr.length > 0) {
+//     returnArr.push(origArr);
+//   }
+
+//   return returnArr;
+// }
+
+// Solution 2 - Use splice instead of for loop (cleaner)
+function chunk(array, size) {
+  const origArr = array;
+  const returnArr = [];
+
+  while (origArr.length >= size) {
+    returnArr.push(origArr.splice(0, size));
+  }
+
+  if (origArr.length > 0) {
+    returnArr.push(origArr);
+  }
+
+  return returnArr;
+}
+
+// Solution 3 - Use slice instead of splice so that the original array is not modified. Adjust the index then in the while...loop
+
+console.log(chunk([1, 2, 3, 4], 2));
+console.log(chunk([1, 2, 3, 4, 5], 2));
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3));
+console.log(chunk([1, 2, 3, 4, 5], 4));
+console.log(chunk([1, 2, 3, 4, 5], 10));
 
 module.exports = chunk;
