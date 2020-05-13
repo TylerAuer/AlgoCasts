907cd204b176f6b1122c0917c580cd2f8a01e152
Tyler Auer <Tyler.Auer@gmail.com>
2020-05-13 15:48:05 -0700

    Add recursive solution to steps

--- exercises/9-steps/index.js
+++ exercises/9-steps/index.js
@@ -18,16 +18,32 @@
 //       '####'
 
 // 1 - 2x For loops
-function steps(n) {
-  for (let lvl = 1; lvl <= n; lvl++) {
-    let nextLine = "";
-    for (let i = 1; i <= n; i++) {
-      i <= lvl ? (nextLine += "#") : (nextLine += " ");
-    }
-    console.log(nextLine);
-  }
-}
+// function steps(n) {
+//   for (let lvl = 1; lvl <= n; lvl++) {
+//     let nextLine = "";
+//     for (let i = 1; i <= n; i++) {
+//       i <= lvl ? (nextLine += "#") : (nextLine += " ");
+//     }
+//     console.log(nextLine);
+//   }
+// }
 
 // 2 - recursion
+function steps(n) {
+  const recSteps = (lvl, len) => {
+    const pound = "#";
+    const space = " ";
+
+    console.log(pound.repeat(len - lvl + 1) + space.repeat(lvl - 1));
+    // Stop if on level 1, else recursively call recSteps
+
+    if (lvl > 1) {
+      recSteps(lvl - 1, len);
+    }
+    return;
+  };
+
+  recSteps(n, n);
+}
 
 module.exports = steps;
