3ad1b72cdf224d29c3be5cdc6bc7a70e4d2d1551
Tyler Auer <Tyler.Auer@gmail.com>
2020-05-15 08:55:38 -0700

    Solve Fib recursively

--- exercises/13-fib/index.js
+++ exercises/13-fib/index.js
@@ -9,26 +9,39 @@
 //   fib(4) === 3
 
 // Iterative Solution
-function fib(n) {
-  // returns 0, 1 for fib(0) and fib(1) respectively
-  if (n === 0) {
-    return 0;
-  } else if (n === 1) {
-    return 1;
-  }
+// function fib(n) {
+//   // returns 0, 1 for fib(0) and fib(1) respectively
+//   if (n === 0) {
+//     return 0;
+//   } else if (n === 1) {
+//     return 1;
+//   }
 
-  let prevFib = 0;
-  let curFib = 1;
+//   let prevFib = 0;
+//   let curFib = 1;
 
-  // iterates forward by adding two previous fibs
-  for (let i = 1; i < n; i++) {
-    [prevFib, curFib] = [curFib, prevFib + curFib];
-  }
+//   // iterates forward by adding two previous fibs
+//   for (let i = 1; i < n; i++) {
+//     [prevFib, curFib] = [curFib, prevFib + curFib];
+//   }
 
-  return curFib;
-}
+//   return curFib;
+// }
 
 // Recursive Solution
-// function fib(n) {}
+function fib(n) {
+  // Base Case:
+  if (n < 2) {
+    return n;
+  }
+
+  // Recursive Case
+  // Calls over and over until reaches fib(0) or fib(1) which
+  // return 0 or 1. Those values are then added (really just counting the fib(1)s)
+  // back up the calls
+  // A tree diagram with fib(5) at the top splitting to
+  // fib(4) and fib(3) illustrates this well
+  return fib(n - 1) + fib(n - 2);
+}
 
 module.exports = fib;
