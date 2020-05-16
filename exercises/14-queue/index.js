cafd52b2c11ecd0a610a429927f6749c8ae19b8e
Tyler Auer <Tyler.Auer@gmail.com>
2020-05-16 14:34:01 -0700

    add implementation of queue from course lecture.

--- exercises/14-queue/index.js
+++ exercises/14-queue/index.js
@@ -8,6 +8,19 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+// Exact copy of the solution from the course
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
 
 module.exports = Queue;
