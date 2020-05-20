409cb2dc5c385ba61f7807f66c5b91d8d7958ed7
Tyler Auer <Tyler.Auer@gmail.com>
2020-05-20 09:39:24 -0700

    implement getFirst() and getLast() in LinkedList

--- exercises/18-linkedlist/index.js
+++ exercises/18-linkedlist/index.js
@@ -14,6 +14,7 @@ class LinkedList {
     this.head = null;
   }
 
+  // inserts new node at head (start) of linked list
   insertFirst(data) {
     // updates the head to a new node where
     // the next parameter is the old head or
@@ -21,16 +22,29 @@ class LinkedList {
     this.head = new Node(data, this.head);
   }
 
+  // returns int of elements in the list
   size() {
     let counter = 0;
     let node = this.head;
-
     while (node) {
       counter++;
       node = node.next;
     }
-
     return counter;
+  }
+
+  // returns first node in the linked list
+  getFirst() {
+    return this.head;
+  }
+
+  // returns last node in the linked list
+  getLast() {
+    let node = this.head;
+    while (node.next != null) {
+      node = node.next;
+    }
+    return node;
   }
 }
 
