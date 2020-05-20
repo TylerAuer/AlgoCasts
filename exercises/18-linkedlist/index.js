352b3f2aaf0d2596999e76b3440d83841b1bcceb
Tyler Auer <Tyler.Auer@gmail.com>
2020-05-20 10:34:42 -0700

    implement clear(), removeFirst(), removeLast(), and insertLast()

--- exercises/18-linkedlist/index.js
+++ exercises/18-linkedlist/index.js
@@ -41,10 +41,52 @@ class LinkedList {
   // returns last node in the linked list
   getLast() {
     let node = this.head;
+    // runs while there is a next Node referenced in the current node
     while (node.next != null) {
       node = node.next;
     }
     return node;
+  }
+
+  // empty list
+  clear() {
+    this.head = null;
+  }
+
+  // removes the first node in the list
+  removeFirst() {
+    this.head = this.head.next || null;
+  }
+
+  // removes the last element
+  removeLast() {
+    // Case of empty list
+    if (!this.head) {
+      return;
+    }
+    // Case of list length 1
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+    // Case of length > 1
+    let previous = this.head;
+    let node = this.head.next;
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+    return;
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+    if (last) {
+      last.next = new Node(data);
+    } else {
+      this.head = new Node(data);
+    }
   }
 }
 
