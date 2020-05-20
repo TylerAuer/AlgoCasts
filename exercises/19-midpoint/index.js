28e9da1b1828b799d12039ad1ef8a528fb805032
Tyler Auer <Tyler.Auer@gmail.com>
2020-05-20 14:19:01 -0700

    solved midpoint of linked list

--- exercises/19-midpoint/index.js
+++ exercises/19-midpoint/index.js
@@ -12,6 +12,23 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+// list is a LinkedList using the implementation from 18-linkedlist
+// O(1.5n)
+function midpoint(list) {
+  // if list is empty
+  if (!list.head) {
+    return null;
+  }
+
+  let slow = list.head;
+  let fast = list.head;
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+}
 
 module.exports = midpoint;
