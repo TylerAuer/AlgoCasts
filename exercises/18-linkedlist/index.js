199c0210abe6bd3f6bd55448a269086cfdefbaed
Tyler Auer <Tyler.Auer@gmail.com>
2020-05-20 09:34:03 -0700

    implement Node and Linked List with methods inserFirst() and size()

--- exercises/18-linkedlist/index.js
+++ exercises/18-linkedlist/index.js
@@ -2,8 +2,36 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    // updates the head to a new node where
+    // the next parameter is the old head or
+    // null if there was no previous head
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+
+    return counter;
+  }
+}
 
 module.exports = { Node, LinkedList };
