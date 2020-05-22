4ecd7eed74d668c4270885ddb386f897c35de002
Tyler Auer <Tyler.Auer@gmail.com>
2020-05-21 21:26:09 -0700

    solve binary search tree validation with min-max approach

--- exercises/25-validate/index.js
+++ exercises/25-validate/index.js
@@ -5,6 +5,26 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  // Base Case
+  if (max && node.data > max) {
+    return false;
+  }
+  if (min && node.data < min) {
+    return false;
+  }
+
+  // Move left, adjust max
+  if (node.left) {
+    return validate(node.left, min, node.data);
+  }
+
+  // Move right, adjust min
+  if (node.right) {
+    return validate(node.right, node.data, max);
+  }
+
+  return true;
+}
 
 module.exports = validate;
